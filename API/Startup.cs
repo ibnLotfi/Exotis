cab4655534833e2c1c5cad83f1ec1969029f7720
ibnLotfi <ibnlotfi@hotmail.fr>
2020-08-07 17:09:48 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,6 +26,7 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IProductRepository, ProductRepository>();
             
         }
 
